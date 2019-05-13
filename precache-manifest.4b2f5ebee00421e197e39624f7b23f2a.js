9c431d73c0c11f98d1524ddf5b65117eeb5ec092
Wojciech Baczyński <wp.baczynski@gmail.com>
2019-05-13 13:32:41 +0200

    Updates

---
+++ precache-manifest.4b2f5ebee00421e197e39624f7b23f2a.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ef47ceb909587a927fd989c4dac6ce73",
+    "url": "/github-repos/index.html"
+  },
+  {
+    "revision": "08d4318e99b062fe6b4a",
+    "url": "/github-repos/static/css/main.6fdfdfd2.chunk.css"
+  },
+  {
+    "revision": "787ae1f96fb69829bb45",
+    "url": "/github-repos/static/js/2.78eacecc.chunk.js"
+  },
+  {
+    "revision": "08d4318e99b062fe6b4a",
+    "url": "/github-repos/static/js/main.70228327.chunk.js"
+  },
+  {
+    "revision": "e5b0a2155358fe9f5628",
+    "url": "/github-repos/static/js/runtime~main.5be2ab4f.js"
+  },
+  {
+    "revision": "02b1bb307b5f3ebaf3df8c2777fb9f4c",
+    "url": "/github-repos/static/media/Jost-100-Italic.02b1bb30.ttf"
+  },
+  {
+    "revision": "6cc49c2b56b13f48fc67e1f7a6fe2bbd",
+    "url": "/github-repos/static/media/Jost-100.6cc49c2b.ttf"
+  },
+  {
+    "revision": "b6ab188fc2d69012a4e15ce75144dc63",
+    "url": "/github-repos/static/media/Jost-200-Italic.b6ab188f.ttf"
+  },
+  {
+    "revision": "961d6b00b110a045517a2770ebb42253",
+    "url": "/github-repos/static/media/Jost-200.961d6b00.ttf"
+  },
+  {
+    "revision": "2b8b559dce3e89b3109a936fbb83f718",
+    "url": "/github-repos/static/media/Jost-300-Italic.2b8b559d.ttf"
+  },
+  {
+    "revision": "d31e2e437e383a1eb550c45207b6c2ef",
+    "url": "/github-repos/static/media/Jost-300.d31e2e43.ttf"
+  },
+  {
+    "revision": "926e4355911ed68683bb1806a3380e78",
+    "url": "/github-repos/static/media/Jost-400-Italic.926e4355.ttf"
+  },
+  {
+    "revision": "49cbd928da11ec96d360be6a691431c6",
+    "url": "/github-repos/static/media/Jost-400.49cbd928.ttf"
+  },
+  {
+    "revision": "df29c4463e6c1aa79bb533d021ba64b5",
+    "url": "/github-repos/static/media/Jost-500-Italic.df29c446.ttf"
+  },
+  {
+    "revision": "90f3226adcb294f01692bfb3d0e9bb96",
+    "url": "/github-repos/static/media/Jost-500.90f3226a.ttf"
+  },
+  {
+    "revision": "29eda5155a0dcdfbc6a89828ea3ef6e8",
+    "url": "/github-repos/static/media/Jost-600-Italic.29eda515.ttf"
+  },
+  {
+    "revision": "b638ab720f8ba66039cdb02a21c2f16f",
+    "url": "/github-repos/static/media/Jost-600.b638ab72.ttf"
+  },
+  {
+    "revision": "8e85c7875c78aff4016b7447cef9754e",
+    "url": "/github-repos/static/media/Jost-700-Italic.8e85c787.ttf"
+  },
+  {
+    "revision": "48900fc9fb6e9712bcb333419bbe8b62",
+    "url": "/github-repos/static/media/Jost-700.48900fc9.ttf"
+  },
+  {
+    "revision": "70133065432b42eda549ce1d77ed72af",
+    "url": "/github-repos/static/media/Jost-800-Italic.70133065.ttf"
+  },
+  {
+    "revision": "04b84afbc76492e9a51dae1eeaa6e7c8",
+    "url": "/github-repos/static/media/Jost-800.04b84afb.ttf"
+  },
+  {
+    "revision": "de96c6a37ace57cacde209019952b08e",
+    "url": "/github-repos/static/media/Jost-900-Italic.de96c6a3.ttf"
+  },
+  {
+    "revision": "12e7a76f597448031e7a8d45bcb4b304",
+    "url": "/github-repos/static/media/Jost-900.12e7a76f.ttf"
+  },
+  {
+    "revision": "d14a04844ca29b5b3994f22631295ebf",
+    "url": "/github-repos/static/media/arrow-left-circle.d14a0484.svg"
+  },
+  {
+    "revision": "bdbb0ef6a4d501b308160ed3143604bb",
+    "url": "/github-repos/static/media/arrow-right-circle.bdbb0ef6.svg"
+  },
+  {
+    "revision": "35c453808f537a8d964953195e82c86d",
+    "url": "/github-repos/static/media/folder-minus.35c45380.svg"
+  },
+  {
+    "revision": "819e77a4dd1b924f84a5c1d4610d426d",
+    "url": "/github-repos/static/media/folder-plus.819e77a4.svg"
+  },
+  {
+    "revision": "3812d86fa4f162846016e03340c94427",
+    "url": "/github-repos/static/media/leaguespartan-bold.3812d86f.woff2"
+  },
+  {
+    "revision": "5d202a7f44e80632637a8f06f374147e",
+    "url": "/github-repos/static/media/leaguespartan-bold.5d202a7f.eot"
+  },
+  {
+    "revision": "600a5766ca3210255986a3779c829dd4",
+    "url": "/github-repos/static/media/leaguespartan-bold.600a5766.svg"
+  },
+  {
+    "revision": "7cc8cbbd7a330c0d1e7c08c6d62711e4",
+    "url": "/github-repos/static/media/leaguespartan-bold.7cc8cbbd.ttf"
+  },
+  {
+    "revision": "de124ab4fe636436b7f9d3b1f5ac3eed",
+    "url": "/github-repos/static/media/leaguespartan-bold.de124ab4.woff"
+  },
+  {
+    "revision": "ac8ea2135e1379656f02ca2eea26cc87",
+    "url": "/github-repos/static/media/star.ac8ea213.svg"
+  }
+]);
